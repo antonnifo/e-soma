277b80fc19eca75c4de46adddb0b33de1748f680
antonnifo <antonnifo@live.com>
2023-05-04 15:24:15 +0300

    add required permission to all views

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.list import ListView
 from .models import Course
 from django.views.generic.edit import CreateView, \
-                                        UpdateView, DeleteView
+    UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    PermissionRequiredMixin
 
 
 class OwnerMixin:
@@ -12,9 +14,11 @@ class OwnerMixin:
     objects by the owner attribute to retrieve objects that belong to the 
     current user (request.user).
     '''
+
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
+
 
 class OwnerEditMixin:
     '''
@@ -26,35 +30,40 @@ class OwnerEditMixin:
     the user to success_url. You override this method to automatically set
     the current user in the owner attribute of the object being saved.
     By doing so, you set the owner for an object automatically when it is saved.
-    '''    
+    '''
+
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
-        
-class OwnerCourseMixin(OwnerMixin):
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
+
+
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
 
 
-class ManageCourseListView(OwnerCourseMixin,ListView):
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = 'courses.change_course'
+
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
 
 
-
-
-
+    
 

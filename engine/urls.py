0ea7e7606ab00ed035652ab73c956cf277aef969
antonnifo <antonnifo@live.com>
2023-05-10 11:06:51 +0300

    include authentification views

--- engine/urls.py
+++ engine/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from engine.settings import base
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), 
+          name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), 
+          name='logout'),    
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
 ] + static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
+

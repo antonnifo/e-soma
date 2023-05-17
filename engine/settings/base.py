0e9e7c760c9648168b975bc776b2c15db8b92600
antonnifo <antonnifo@live.com>
2023-05-17 11:27:58 +0300

    register students app

--- engine/settings/base.py
+++ engine/settings/base.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 INSTALLED_APPS = [
     'courses',
     'jazzmin',
+    'students',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

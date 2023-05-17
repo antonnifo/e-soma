899197663218d6239cf39efc8bff5a6c734c84a1
antonnifo <antonnifo@live.com>
2023-05-17 11:13:26 +0300

    include enrollment form in the context 4rendering template

--- courses/views.py
+++ courses/views.py
@@ -13,6 +13,7 @@ from django.views.generic.list import ListView
 
 from .forms import ModuleFormSet
 from .models import Content, Course, Module, Subject
+from students.forms import CourseEnrollForm
 
 
 class OwnerMixin:
@@ -198,6 +199,13 @@ class CourseListView(TemplateResponseMixin, View):
 
 
 class CourseDetailView(DetailView):
+
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+                                   initial={'course':self.object})
+        return context    
 

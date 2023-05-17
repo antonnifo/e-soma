ef14dbbd1a2f5407a1a58f0caa234ecb9232681e
antonnifo <antonnifo@live.com>
2023-05-17 11:41:05 +0300

    include students app urls

--- engine/urls.py
+++ engine/urls.py
@@ -24,11 +24,12 @@ from engine.settings import base
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), 
-          name='login'),
+                                 name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), 
-          name='logout'),    
+                                name='logout'),    
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
+    path('students/', include('students.urls')),
 ] + static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
 

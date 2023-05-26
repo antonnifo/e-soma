bf7f8d7d5765c27ad4fbb0554da8f5227e0861e4
antonnifo <antonnifo@live.com>
2023-05-26 11:10:41 +0300

    include api views

--- engine/urls.py
+++ engine/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api/', include('courses.api.urls', namespace='api')),
     
     ] + static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
 
